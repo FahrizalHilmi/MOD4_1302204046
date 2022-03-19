df4ef5cfeb004a7f0f8218baef129d6b83a92e3b
rizal005 <fahrizalhilmi2002@gmail.com>
2022-03-19 17:16:43 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MOD4_1302204046_AQL
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           
+            Penjumlahan.JumlahTigaAngka<int>(13, 02, 20);
+            SimpleDataBase<int> objData1 = new SimpleDataBase<int>();
+            objData1.AddNewData(13);
+            objData1.AddNewData(02);
+            objData1.AddNewData(20);
+            objData1.PrintAllData();
+        }
+    }
+}
